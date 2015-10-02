ec24f0ebb814920f9e8767f175ffcf9d181a0eed
Nicholas Smith <npsmith1990@gmail.com>
2015-10-02 12:17:09 -0500

    fix alignment

--- js/app.js
+++ js/app.js
@@ -85,51 +85,51 @@ function update(dt) {
   // equation: 1-.993^gameTime
   if(Math.random() < 1 - Math.pow(.993, gameTime)) {
     enemies.push({
-      pos: [canvas.width,
-        Math.random() * (canvas.height - 39)], sprite: new Sprite('img/sprites.png', [0, 78], [80, 39], 6, [0, 1, 2, 3, 2, 1])
+      pos: [canvas.width, Math.random() * (canvas.height - 39)], sprite: new Sprite('img/sprites.png', [0, 78], [80, 39], 6, [0, 1, 2, 3, 2, 1])
     });
   }
-    checkCollisions();
-    scoreEl.innerHTML = score;
+  checkCollisions();
+  scoreEl.innerHTML = score;
 }
 
-  // User input
-  function handleInput(dt) {
-    if(input.isDown('DOWN') || input.isDown('s')) {
-      player.pos[1] += playerSpeed * dt;
-    }
-    if(input.isDown('UP') || input.isDown('w')) {
-      player.pos[1] -= playerSpeed * dt;
-    }
-
-    if(input.isDown('LEFT') || input.isDown('a')) {
-      player.pos[0] -= playerSpeed * dt;
-    }
-
-    if(input.isDown('RIGHT') || input.isDown('d')) {
-      player.pos[0] += playerSpeed * dt;
-    }
-
-    if (input.isDown('SPACE') && !isGameOver && Date.now() - lastFire > 100) {
-      var x = player.pos[0] + player.sprite.size[0] / 2;
-      var y = player.pos[1] + player.sprite.size[1] / 2;
-
-      bullets.push({
-        pos: [x, y],
-        dir: 'forward',
-        sprite: new Sprite('img/sprites.png', [0, 39], [18,8])
-      });
-      bullets.push({
-        pos: [x, y],
-        dir: 'up',
-        sprite: new Sprite('img/sprites.png', [0, 50], [9, 5])
-      });
-      bullets.push({
-        pos: [x, y],
-        dir: 'down',
-        sprite: new Sprite('img/sprites.png', [0, 60], [9, 5])
-      });
-
-      lastFire = Date.now();
-    }
+// User input
+function handleInput(dt) {
+  if(input.isDown('DOWN') || input.isDown('s')) {
+    player.pos[1] += playerSpeed * dt;
   }
+
+  if(input.isDown('UP') || input.isDown('w')) {
+    player.pos[1] -= playerSpeed * dt;
+  }
+
+  if(input.isDown('LEFT') || input.isDown('a')) {
+    player.pos[0] -= playerSpeed * dt;
+  }
+
+  if(input.isDown('RIGHT') || input.isDown('d')) {
+    player.pos[0] += playerSpeed * dt;
+  }
+
+  if (input.isDown('SPACE') && !isGameOver && Date.now() - lastFire > 100) {
+    var x = player.pos[0] + player.sprite.size[0] / 2;
+    var y = player.pos[1] + player.sprite.size[1] / 2;
+
+    bullets.push({
+      pos: [x, y],
+      dir: 'forward',
+      sprite: new Sprite('img/sprites.png', [0, 39], [18,8])
+    });
+    bullets.push({
+      pos: [x, y],
+      dir: 'up',
+      sprite: new Sprite('img/sprites.png', [0, 50], [9, 5])
+    });
+    bullets.push({
+      pos: [x, y],
+      dir: 'down',
+      sprite: new Sprite('img/sprites.png', [0, 60], [9, 5])
+    });
+
+    lastFire = Date.now();
+  }
+}
